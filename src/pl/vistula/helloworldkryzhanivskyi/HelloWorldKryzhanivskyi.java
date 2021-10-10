98d42c7570d2137467c7ea8c7322a695f0c5bd0f
Oleksandr Kryzhanivskyi <okryzha1@stu.vistula.edu.pl>
2021-10-10 23:25:37 +0200

    add 2 lines

--- src/pl/vistula/helloworldkryzhanivskyi/HelloWorldKryzhanivskyi.java
+++ src/pl/vistula/helloworldkryzhanivskyi/HelloWorldKryzhanivskyi.java
@@ -3,5 +3,7 @@ package pl.vistula.helloworldkryzhanivskyi;
 public class HelloWorldKryzhanivskyi {
     public static void main(String[] args) {
         System.out.println("Hello Vistula Kryzhanivskyi");
+        System.out.println("Dzień dobry!!!");
+        System.out.println("Miłego dnia!!!");
     }
 }
