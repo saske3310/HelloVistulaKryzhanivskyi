10760cf72c57ace11f6458e5aa6f0267ca98235d
Oleksandr Kryzhanivskyi <okryzha1@stu.vistula.edu.pl>
2021-10-10 22:10:55 +0200

    add commit

---
+++ src/pl/vistula/helloworldkryzhanivskyi/HelloWorldKryzhanivskyi.java
@@ -0,0 +1,7 @@
+package pl.vistula.helloworldkryzhanivskyi;
+
+public class HelloWorldKryzhanivskyi {
+    public static void main(String[] args) {
+        System.out.println("Hello Vistula Kryzhanivskyi");
+    }
+}
